f88b3ec2a680180caa1afe94535a4f4df425891e
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-04-26 13:07:52 -0400

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,41 @@
 $(document).ready(function() {
 
-var count= 30;
-var counter= setInterval(timer, 1000);
-var choice[];
 
- var timer = function() {
-	count = count-1;
-	if (count <= 0) {
-		clearInterval(counter);
-		return;
-	}
-	document.getElementById('timer').innerHTML='Time ' + count;
+var timeLeft = 30;
+var elem = document.getElementById('#timer');
+
+var timerId = setInterval(countdown, 1000);
+
+$('#timer').empty();
+$('#timer').append(timeLeft);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    alert('Times up!');
+
+  } else {
+    elem.innerHTML = timeLeft + ' Time Remaining';
+    timeLeft--;
+  }
 }
-
 })
+
+
+//var triviaQuestions = [{
+//    question: 'Which basketball team won the 2016 NBA Championship?',
+//    choices: ['Warriors', 'Cavaliers', 'Spurs', 'Pacers',]
+   // answer: 1
+
+//},{
+//    question: 'Which of these Breaking Bad Characters got there own spinoff TV show after the series ended?',
+//    choices: ['Jesse Pinkman', 'Gus Fring', 'Saul Goodman', 'Hank Schrader',]
+   // answer: 2
+//},{
+//    question: 'Which of these fish are known for being the fastest swimming?',
+//    choices: ['Shark', 'Swordfish', 'Marlin', 'Sailfish',]
+   // answer: 3
+//}],
 
 
 
