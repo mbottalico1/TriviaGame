740ddba2bc5a1e10755365582fb6ae084ba870b1
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-04-26 15:59:56 -0400

    fixed timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,25 +2,24 @@ $(document).ready(function() {
 
 
 var timeLeft = 30;
-var elem = document.getElementById('#timer');
+var $elem = $('#timer');
 
 var timerId = setInterval(countdown, 1000);
 
-$('#timer').empty();
-$('#timer').append(timeLeft);
+  $elem.empty();
+  $elem.text(timeLeft);
 
 function countdown() {
-  if (timeLeft == 0) {
+ if (timeLeft == 0) {
     clearTimeout(timerId);
     alert('Times up!');
 
-  } else {
-    elem.innerHTML = timeLeft + ' Time Remaining';
-    timeLeft--;
-  }
-}
+} else {
+       $elem.text(timeLeft);
+       timeLeft--;
+     }
+   }
 })
-
 
 //var triviaQuestions = [{
 //    question: 'Which basketball team won the 2016 NBA Championship?',
