14fc65634d52049b70eaf563dc1c5472612b6b2c
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-04-27 10:56:06 -0400

    ..

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,23 +21,4 @@ function countdown() {
    }
 })
 
-//var triviaQuestions = [{
-//    question: 'Which basketball team won the 2016 NBA Championship?',
-//    choices: ['Warriors', 'Cavaliers', 'Spurs', 'Pacers',]
-   // answer: 1
-
-//},{
-//    question: 'Which of these Breaking Bad Characters got there own spinoff TV show after the series ended?',
-//    choices: ['Jesse Pinkman', 'Gus Fring', 'Saul Goodman', 'Hank Schrader',]
-   // answer: 2
-//},{
-//    question: 'Which of these fish are known for being the fastest swimming?',
-//    choices: ['Shark', 'Swordfish', 'Marlin', 'Sailfish',]
-   // answer: 3
-//}],
-
-
-
-
-
 
